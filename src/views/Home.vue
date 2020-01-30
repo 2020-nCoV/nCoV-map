e854987de29587703d5f05e1e1b8cd8fd19d5970
yc111 <yinchuan_111@163.com>
2020-01-30 17:08:54 +0800

    init project

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="nCoVlogo" src="../assets/logo.png">
+    <Map msg="Welcome to nCoV-map App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Map from '@/components/Map.vue';
+
+export default {
+  name: 'home',
+  components: {
+    Map,
+  },
+};
+</script>
